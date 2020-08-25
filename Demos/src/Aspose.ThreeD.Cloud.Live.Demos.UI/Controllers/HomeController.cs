94beaba8d8a065e384b2facb44a5fc4d1da99363
Naeem Akram <naeem.akram@aspose.com>
2020-08-25 11:40:39 +0500

    updated web.config and default page

--- Demos/src/Aspose.ThreeD.Cloud.Live.Demos.UI/Controllers/HomeController.cs
+++ Demos/src/Aspose.ThreeD.Cloud.Live.Demos.UI/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace Aspose.ThreeD.Cloud.Live.Demos.UI.Controllers
 		public ActionResult Default()
 		{
 			ViewBag.PageTitle = "C# REST API to Perform 3D Modeling, Conversion (in the Cloud)";
-			ViewBag.MetaDescription = "spose.3D Cloud for cURL helps developers use easy cURL commands to create &amp; process 3D models, objects and 3D entities (3D Box, 3D Cylinder, 3D Sphere, 3D Torus, 3D Plane) in the cloud. Perform parametric modeling of 3D objects. Extract 3D contents from a 3D PDF file. Use cURL to convert 3D objects to supports formats in the cloud.";
+			ViewBag.MetaDescription = "Aspose.3D Cloud for cURL helps developers use easy cURL commands to create &amp; process 3D models, objects and 3D entities (3D Box, 3D Cylinder, 3D Sphere, 3D Torus, 3D Plane) in the cloud. Perform parametric modeling of 3D objects. Extract 3D contents from a 3D PDF file. Use cURL to convert 3D objects to supports formats in the cloud.";
 			var model = new LandingPageModel(this);
 
 			return View(model);
